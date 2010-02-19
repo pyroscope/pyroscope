5b6968d75ba496f6e7644b36bbeba1ba5d51315c
pyroscope <pyroscope.project@gmail.com>
2010-02-19 18:51:14 +0000

    generic bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+# This script has to be sourced in a shell and is thus NOT executable.
+virtualenv --no-site-packages . || return 1
+. bin/activate || return 1
+
+# tools
+easy_install -U "setuptools==0.6c11" || return 1
+easy_install -U "paver>=1.0.1" || return 1
+easy_install -U "epydoc>=3.0.1" || return 1
+easy_install -U "nose>=0.10.3" || return 1
+easy_install -U "coverage==2.80" || return 1
+easy_install -U "pylint>=0.18.0" || return 1
+easy_install -U "yolk>=0.4.1" || return 1
+easy_install -U "PasteScript>=1.7.3" || return 1
+
+export DEBFULLNAME=pyroscope
+export DEBEMAIL=pyroscope.project@gmail.com
+
