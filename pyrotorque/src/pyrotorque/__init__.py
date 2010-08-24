65459e377873b1ccc6e124b80671df657dc00aed
pyroscope <pyroscope.project@gmail.com>
2010-08-24 18:31:12 +0000

    TODO

--- pyrotorque/src/pyrotorque/__init__.py
+++ pyrotorque/src/pyrotorque/__init__.py
@@ -17,3 +17,14 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
+# TODO: cyclic refresh
+#   - config param "update_cycle" for length of update cycle (in sec)
+#   - get initial list of hashes
+#   - get bursts of hashes, each "update_cycle / burst_size" secs
+#   - watch "active" and "incomplete" view and update those more frequently (list of "hot" hashes)
+#   - if hash disappears (i.e. we get a xmlrpc fault), remove hash from list
+#   - do a full referesh of the list of hashes from time ti time (once each cycle?)
+#
+#   - put list of hashes we change into "dirty" list
+#   - re-get dirty hashes on read access
+
