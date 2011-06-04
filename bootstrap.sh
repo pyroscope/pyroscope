5490d13e917511cf8a6cb532b05b5732279675e9
pyroscope <pyroscope.project@gmail.com>
2011-06-04 10:41:37 +0000

    more automatic source installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 # This script has to be sourced in a shell and is thus NOT executable.
-test ! -f ./bin/activate || return 0
-deactivate 2>/dev/null || true
-virtualenv --no-site-packages . || return 1
+if test ! -f ./bin/activate; then
+    deactivate 2>/dev/null || true
+    virtualenv --no-site-packages . || return 1
+fi
 
 export DEBFULLNAME=pyroscope
 export DEBEMAIL=pyroscope.project@gmail.com
 
-cat >>bin/activate <<EOF
+grep DEBFULLNAME bin/activate || cat >>bin/activate <<EOF
 export DEBFULLNAME=$DEBFULLNAME
 export DEBEMAIL=$DEBEMAIL
 EOF
@@ -14,10 +15,10 @@ EOF
 . bin/activate || return 1
 
 # tools
-easy_install -U "setuptools==0.6c11" || return 1
+easy_install -U "setuptools>=0.6c11" || return 1
 easy_install -U "paver>=1.0.1" || return 1
-easy_install -U "nose>=1.0" || return 1
-easy_install -U "coverage>=3.4" || return 1
+##easy_install -U "nose>=1.0" || return 1
+##easy_install -U "coverage>=3.4" || return 1
 easy_install -U "yolk>=0.4.1" || return 1
 ##easy_install -U "PasteScript>=1.7.3" || return 1
 
--- update-to-head.sh
+++ update-to-head.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
+install_venv() {
+    venv='https://github.com/pypa/virtualenv/raw/master/virtualenv.py'
+    python -c "import urllib2; open('venv.py','w').write(urllib2.urlopen('$venv').read())"
+    deactivate 2>/dev/null || true
+    python venv.py --no-site-packages .
+}
+
 set -e
 cd $(dirname "$0")
 echo "Updating your installation..."
 
 # Ensure virtualenv is there
-test -f bin/activate || { echo "ERROR: No virtualenv found (./bin/activate missing)"; exit 1; }
+test -f bin/activate || install_venv
 
-# Get pyrobase initially, for old installations
+# Get pyrobase initially, for old or yet incomplete installations
 test -d pyrobase || { echo "Getting pyrobase..."; git clone "git://github.com/pyroscope/pyrobase.git" pyrobase; }
 
 # Update source
